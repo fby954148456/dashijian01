bdc1df6ddba3d7a55ca40baee45c0423ad4a139d
fengbingyang <954148456@qq.com>
2020-10-14 14:48:47 +0800

    修改一下

--- assets/js/index.js
+++ assets/js/index.js
@@ -55,7 +55,7 @@ function getUserInfo() {
 // 渲染用户的头像
 function renderAvatar(user) {
   // 1. 获取用户的名称
-  var name = user.username;
+  var name = user.nickname || user.username;
   // 2. 设置欢迎的文本
   $("#welcome").html("欢迎&nbsp;&nbsp;" + name);
   // 3. 按需渲染用户的头像
