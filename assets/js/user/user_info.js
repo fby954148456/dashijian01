de45ac36320f525a3ac9dc8bb835267bae79d7ec
fengbingyang <954148456@qq.com>
2020-10-14 13:33:40 +0800

    修改一下

--- assets/js/user/user_info.js
+++ assets/js/user/user_info.js
@@ -1,3 +1,22 @@
 $(function () {
-    
-})
+  var form = layui.form;
+
+  form.verify({
+    nickname: function (value) {
+      if (value.length > 6) {
+        return "昵称长度必须在 1 ~ 6 个字符之间！";
+      }
+    },
+  });
+  initUserInfo();
+  function initUserInfo() {
+    $.get("/my/userinfo", function (res) {
+      if (res.status !== 0) {
+        return layer.msg("获取用户信息失败！");
+      }
+      // console.log(res)
+      // 调用 form.val() 快速为表单赋值
+      form.val("formUserInfo", res.data);
+    });
+  }
+});
