5a7eef09c175bb00d307117280faf56fb38e271e
fengbingyang <954148456@qq.com>
2020-10-14 15:04:22 +0800

    修改一下

--- assets/js/user/user_info.js
+++ assets/js/user/user_info.js
@@ -1,6 +1,7 @@
 $(function () {
   var form = layui.form;
   var layer = layui.layer;
+
   form.verify({
     nickname: function (value) {
       if (value.length > 6) {
@@ -8,39 +9,50 @@ $(function () {
       }
     },
   });
+
   initUserInfo();
+
+  // 初始化用户的基本信息
   function initUserInfo() {
-    $.get("/my/userinfo", function (res) {
-      if (res.status !== 0) {
-        return layer.msg("获取用户信息失败！");
-      }
-      // console.log(res)
-      // 调用 form.val() 快速为表单赋值
-      form.val("formUserInfo", res.data);
+    $.ajax({
+      method: "GET",
+      url: "/my/userinfo",
+      success: function (res) {
+        if (res.status !== 0) {
+          return layer.msg("获取用户信息失败！");
+        }
+        // console.log(res)
+        // 调用 form.val() 快速为表单赋值
+        form.val("formUserInfo", res.data);
+      },
     });
   }
+
+  // 重置表单的数据
   $("#btnReset").on("click", function (e) {
     // 阻止表单的默认重置行为
     e.preventDefault();
     initUserInfo();
   });
-   $(".layui-form").on("submit", function (e) {
-     // 阻止表单的默认提交行为
-     e.preventDefault();
-     // 发起 ajax 数据请求
-     $.ajax({
-       method: "POST",
-       url: "/my/userinfo",
-       data: $(this).serialize(),
-       success: function (res) {
-         if (res.status !== 0) {
-           return layer.msg("更新用户信息失败！");
-         }
-         layer.msg("更新用户信息成功！");
-         // 调用父页面中的方法，重新渲染用户的头像和用户的信息
-         window.parent.getUserInfo();
-       },
-     });
-   });
+
+  // 监听表单的提交事件
+  $(".layui-form").on("submit", function (e) {
+    // 阻止表单的默认提交行为
+    e.preventDefault();
+    // 发起 ajax 数据请求
+    $.ajax({
+      method: "POST",
+      url: "/my/userinfo",
+      data: $(this).serialize(),
+      success: function (res) {
+        if (res.status !== 0) {
+          return layer.msg("更新用户信息失败！");
+        }
+        layer.msg("更新用户信息成功！");
+        // 调用父页面中的方法，重新渲染用户的头像和用户的信息
+        window.parent.getUserInfo();
+      },
+    });
+  });
 });
 
